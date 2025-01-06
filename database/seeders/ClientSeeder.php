cb1e0960245ec0962c8d54440d8892afc4a8599c
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2025-01-06 09:58:29 +0800

    minor changes on client seeder

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 use App\Models\Client;
-use App\Models\Progress;
+use App\Models\Project;
+use App\Models\ProjectStep;
 use App\Models\Task;
-use App\Models\Document;
+use App\Models\RequiredDocument;
+use App\Models\SubmittedDocument;
 
 class ClientSeeder extends Seeder
 {
@@ -17,16 +19,46 @@ class ClientSeeder extends Seeder
         Client::factory()
             ->count(10)
             ->has(
-                Progress::factory()
-                    ->count(3)
-                    ->has(
-                        Task::factory()
-                            ->count(4)
-                            ->has(
-                                Document::factory()
-                                    ->count(2)
-                            )
-                    )
+                Project::factory()
+                    ->count(2)
+                    ->state(function (array $attributes, Client $client) {
+                        return [
+                            'client_id' => $client->id,
+                        ];
+                    })
+                    ->afterCreating(function (Project $project) {
+                        // Create 4 steps for each project
+                        for ($i = 1; $i <= 4; $i++) {
+                            $step = ProjectStep::factory()->create([
+                                'project_id' => $project->id,
+                                'order' => $i,
+                                'name' => match($i) {
+                                    1 => 'Client Registration',
+                                    2 => 'Document Collection',
+                                    3 => 'Shipping Planning',
+                                    4 => 'Execution',
+                                },
+                            ]);
+
+                            // Create 2-3 tasks for each step
+                            Task::factory()
+                                ->count(rand(2, 3))
+                                ->create([
+                                    'project_step_id' => $step->id,
+                                ]);
+
+                            // Create 1-2 required documents for each step
+                            RequiredDocument::factory()
+                                ->count(rand(1, 2))
+                                ->has(
+                                    SubmittedDocument::factory()
+                                        ->count(rand(0, 1)) // Some documents might not be submitted yet
+                                )
+                                ->create([
+                                    'project_step_id' => $step->id,
+                                ]);
+                        }
+                    })
             )
             ->create();
     }
