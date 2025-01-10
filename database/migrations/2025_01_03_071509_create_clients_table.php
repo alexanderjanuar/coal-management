0a35c7303eefae0f3221016420f02fe3c8135023
Rizky Hamdani <rizkyyhamdani21@gmail.com>
2025-01-10 15:38:42 +0800

    "Added 'NPWP' column to 'clients' table in migration."

--- database/migrations/2025_01_03_071509_create_clients_table.php
+++ database/migrations/2025_01_03_071509_create_clients_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            
             $table->string('NPWP');
             $table->string('KPP')->nullable();
             $table->string('logo')->nullable();
