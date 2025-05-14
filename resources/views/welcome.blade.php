fa9f7a515d788e97e4ad91bf99dd1c6b4e2086e4
Alexander Januar <alexanderjanuar16@gmail.com>
2025-05-14 18:19:59 +0800

    fix: Correct comment formatting in welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        <!-- Styles -->123
+        <!-- Styles -->
+        
         
 
         @vite(['resources/css/app.css','resources/js/app.js'])
